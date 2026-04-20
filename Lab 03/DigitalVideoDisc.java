b06e3580612926eda4e586ca17859a9a18122246
NamKhanh2006 <khanhphung990@gmail.com>
2026-04-20 23:27:25 +0700

    Passing parameter

--- Lab 03/DigitalVideoDisc.java
+++ Lab 03/DigitalVideoDisc.java
@@ -1,10 +1,18 @@
+package hust.soict.dsai.aims.disc;
 
 public class DigitalVideoDisc {
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
+	
 	private String title;
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
+	
+	public int getid() {
+		return id;
+	}
 	
 	public String getTitle() {
 		return title;
@@ -21,18 +29,39 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
 	public DigitalVideoDisc(String title) {
 		super();
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 		this.category = category;
 		this.director = director;
@@ -40,6 +69,8 @@ public class DigitalVideoDisc {
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 		this.category = category;
 		this.director = director;
@@ -47,4 +78,13 @@ public class DigitalVideoDisc {
 		this.cost = cost;
 	}
 
+	@Override
+	public String toString() {
+		return id + ". DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+	}
+	
+	public boolean isMatch(String title) {
+		return this.title != null && this.title.equals(title);
+	}
+	
 }
